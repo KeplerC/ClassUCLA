cf04ec03c862bcfbdb7969465c1d40fbc1d5ebac
Kaiyuan <chenkaiyuan@ucla.edu>
2017-09-20 06:16:33 -0700

    completeFindingNewURL

--- classUCLA/scrap.py
+++ classUCLA/scrap.py
@@ -5,9 +5,7 @@ from bs4 import BeautifulSoup
 
 TERM = '17F'
 
-def get_web_page(index):    
-    URL = 'https://sa.ucla.edu/ro/public/soc/Results?t='+TERM+'&sBy=classidnumber&id='+index+'&undefined=Go&btnIsInIndex=btn_inIndex'
-    
+def get_web_page(URL):    
     try:
         r = requests.get(URL)
     except:
@@ -17,72 +15,27 @@ def get_web_page(index):
         soup = BeautifulSoup(r.text, 'html.parser')
     except:
         print("parser failed")
+    return soup
 
-    #text processing
-    status = ""
-    ostream = ""
-    text = soup.find_all('p') #find all tags
-    flag = -1
-    printed = False #a flag for printing class names
-
-    for i in range(len(text)):
-        string = str(text[i])
-        #print(string)
-        if flag != -1:
-            #print(string)
-            #message parsers
-            try:
-                loc1 = string.find("</i>")+4
-                loc2 = string.find("<br/>")
-                loc3 = string.find("</p>")
-            except:
-                print("cannot parse the website")
-            #output stream
-            if loc2 == -1: #class is cancelled
-                status+=" is cancelled \n"
-            else:
-                msg1 = string[loc1: loc2]
-                msg2 = string[loc2+5: loc3]
-                ostream+=" is " + msg1
-                ostream+=". Its status is "+msg2+"\n"
-            flag = -1
-        if string.find("Lec") != -1:
-            #print(string)
-            i+=1        #jump two lines
-            flag = 0    #found it
-        if string.find("Dis") != -1 and len(string) > 200:
-            #print(string)
-            i+=1        #jump two lines
-            flag = 1    #found it
-        if flag != -1 and printed == False:
-            printed = True
-            try:
-                loc1 = string.find("subj_area_cd=") + 13 #class title
-                loc2 = string.find("%20")
-                loc3 = string.find("catlg_no=") + 10  #class number
-                loc4 = string.find("%20", loc3) #find next after loc3
-            except:
-                print("cannot parse the website")
-            
-            ostream += string[loc1 : loc2]
-            ostream += " "
-            if string[loc3] == '0':
-                loc3 += 1
-            ostream += string[loc3 : loc4]
-            ostream += '\n'
-        if flag != -1:
-            loc1 = string.find("</a>")
-            loc2 = loc1 - 6         
-            if(string[loc2] == '>'):
-                loc2 += 1
-            msg = string[loc2 : loc1]
-            
-            ostream += string [loc2 : loc1]
-    return ostream
-
+def get_class_info_basic(index):
+    #initialize variables
+    #https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=17F&subj_area_cd=MATH%20%20%20&crs_catlg_no=0170A%20%20%20&class_id=262660210&class_no=%20002%20%20
+    #https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=17F&subj_area_cd=MATH%20%20%20&crs_catlg_no=0170A%20%20%20&class_id=262660200&class_no=%20001%20%20
+    #
+    
+    URL = 'https://sa.ucla.edu/ro/public/soc/Results?t='+TERM+'&sBy=classidnumber&id='+index+'&undefined=Go&btnIsInIndex=btn_inIndex'
+    soup = get_web_page(URL)
+    text = str(soup.find_all('p')) #find all tags
+    loc1 = text.find("n<a href=\"") + 10
+    loc2 = text.find("20\" target=") + 2
+    string = "https://sa.ucla.edu" + text[loc1:loc2]
+    string = string.replace("amp;", "")
+    return string
+    
 def main():
-    get_web_page("262660200")
-
+    URL = get_class_info_basic("262660200")
+    #print URL
+    print get_web_page(URL)
 
 if __name__ == "__main__":
     main()
